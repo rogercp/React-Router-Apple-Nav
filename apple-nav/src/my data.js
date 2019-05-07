57dbf3244299b4729733598be6c3c2225b92e6e7
Roger Caballero <rogercp1234@yahoo.com>
2019-05-07 13:57:55 -0700

    added fake data in a file on src

---
+++ apple-nav/src/my data.js
@@ -0,0 +1,71 @@
+const mydata=[
+
+{
+    navName:"phone",
+    items:[
+        {
+        name:"iphone",
+        img:"https://image.shutterstock.com/z/stock-vector-smartphone-icon-vector-illustration-211546936.jpg"
+        },
+        {
+            name:"iphone",
+            img:"https://image.shutterstock.com/z/stock-vector-smartphone-icon-vector-illustration-211546936.jpg"
+            },
+            {
+                name:"iphone",
+                img:"https://image.shutterstock.com/z/stock-vector-smartphone-icon-vector-illustration-211546936.jpg"
+                }  
+    ]
+
+},
+
+{
+    navName:"imac",
+    items:[
+        {
+        name:"imac",
+        img:"https://image.shutterstock.com/z/stock-vector-monitor-in-imac-style-black-color-with-blank-screen-isolated-on-the-grey-background-stock-vector-431937952.jpg"
+        },
+        {
+            name:"imac",
+            img:"https://image.shutterstock.com/z/stock-vector-monitor-in-imac-style-black-color-with-blank-screen-isolated-on-the-grey-background-stock-vector-431937952.jpg"
+            },
+            {
+                name:"imac",
+                img:"https://image.shutterstock.com/z/stock-vector-monitor-in-imac-style-black-color-with-blank-screen-isolated-on-the-grey-background-stock-vector-431937952.jpg"
+                }
+    ]
+
+},
+{
+    navName:"macbook",
+    items:[
+        {
+        name:"macbook",
+        img:"https://image.shutterstock.com/z/stock-vector-realistic-silver-white-notebook-with-blank-screen-and-shadow-inch-scalable-laptop-can-use-1152406718.jpg"
+        },
+        {
+            name:"macbook",
+            img:"https://image.shutterstock.com/z/stock-vector-realistic-silver-white-notebook-with-blank-screen-and-shadow-inch-scalable-laptop-can-use-1152406718.jpg"
+            }
+    ]
+
+},
+{
+    navName:"ipad",
+    items:[
+        {
+        name:"ipad",
+        img:"https://image.shutterstock.com/image-vector/realistic-tablet-pc-computer-blank-450w-519402991.jpg"
+        },
+        {
+            name:"ipad",
+            img:"https://image.shutterstock.com/image-vector/realistic-tablet-pc-computer-blank-450w-519402991.jpg"
+            }
+    ]
+
+}
+
+
+
+]
